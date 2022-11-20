1d64ec8a26bd34014571b3d4bf05e97075ff2549
SlickNick99 <59777492+SlickNick99@users.noreply.github.com>
2022-11-19 19:59:09 -0800

    first commit

---
+++ 01-Fundamentals-Part-1/starter/app.js
@@ -0,0 +1,122 @@
+// let js = "amazing";
+// if (js === "amazing") alert("Javascript is fun!");
+
+// let firstName = "Nick";
+// console.log(firstName);
+
+// let lastName = "Velazquez";
+// console.log(firstName + lastName);
+
+// let javascriptIsFun = true;
+
+// console.log(javascriptIsFun);
+
+// Math operators
+// let currentYear = 2022;
+// const ageSarah = currentYear - 2004;
+// const ageNick = currentYear - 1999;
+// const firstName = "Nick";
+// const lastName = "Velazquez";
+// const isFullAge = ageSarah >= 18;
+// console.log(ageNick, ageSarah);
+
+// console.log(firstName + " " + lastName);
+
+// // Assignment operators
+// let x = 10 + 5;
+// x += 10; // x = x + 10
+
+// x *= 4; // x = x * 4 = 100
+
+// x++; // x = x + 1
+
+// x--; // x = x - 1
+// console.log(x);
+
+// // Comparison operators
+// // >, <, >=, <=
+
+// console.log(ageNick > ageSarah);
+// console.log(ageSarah >= 18);
+
+// let markWeight1 = 172;
+// let markHeight1 = 5.5;
+
+// let johnWeight1 = 209;
+// let johnHeight1 = 6.4;
+
+// let markBmi1 = markWeight1 / markHeight1 ** 2;
+
+// let johnBmi1 = johnWeight1 / johnHeight1 ** 2;
+
+// let markHigherBmi1 = markBmi1 > johnBmi1;
+// let johnHigherBmi1 = johnBmi1 > markBmi1;
+
+// if (markHigherBmi1 === true) {
+//   console.log("Marks BMI is higher than Johns");
+// } else {
+//   console.log("johns BMI is higher than Marks");
+// }
+// console.log(markBmi1);
+// console.log(johnBmi1);
+// console.log(johnHigherBmi1);
+
+const markWeight2 = 209;
+const markHeight2 = 6.1;
+
+const johnWeight2 = 187;
+const johnHeight2 = 5.7;
+
+const markBmi2 = markWeight2 / markHeight2 ** 2;
+
+const johnBmi2 = johnWeight2 / johnHeight2 ** 2;
+
+const markHigherBmi2 = markBmi2 > johnBmi2;
+const johnHigherBmi2 = johnBmi2 > markBmi2;
+
+console.log(`marks bmi is ${markBmi2}`);
+console.log(`Johns bmi is ${johnBmi2}`);
+if (markHigherBmi2) {
+  console.log("Marks BMI is higher than Johns");
+} else {
+  console.log("johns BMI is higher than Marks");
+}
+
+console.log(johnHigherBmi2);
+
+// const firstName = "Nick";
+// const job = "teacher";
+// const birthYear = 1999;
+// const currentYear = 2022;
+// const age = currentYear - 1999;
+
+// const nick = "I'm " + firstName + ", a " + age + " year old " + job;
+
+// console.log(nick);
+
+// const nickNew = `I'm ${firstName}, a ${age} year old ${job}`;
+// console.log(nickNew);
+
+// const age = 15;
+// const isOldEnough = age >= 18;
+
+// if (isOldEnough) {
+//   console.log(`Sarah can get her driving license`);
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(
+//     `Sarah cannot get her driving license she has ${yearsLeft} years left in order to get her license`
+//   );
+// }
+
+// const birthYear = 2021;
+
+// let century;
+
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+
+// console.log(century);
---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,34 @@
+// let country = "America";
+// let continent = "North America";
+// let population = 12;
+// const americaInHalf = population / 2;
+// let islandQuestion = "is your country an island?";
+// isIsland = false;
+
+// const language = "english";
+
+// console.log(islandQuestion, isIsland, country, language, continent, population);
+
+// console.log(
+//   country +
+//     " is in " +
+//     continent +
+//     " and its " +
+//     population +
+//     " people speak " +
+//     language
+// );
+
+// console.log(
+//   `${country} is in ${continent} and its ${population} million people speak ${language}`
+// );
+
+// console.log(americaInHalf);
+// const countryAvgPop = 33;
+// const countryAvg = countryAvgPop - population;
+
+// if (population >= countryAvg) {
+//   console.log(`${country}'s population is greater than 33 million`);
+// } else {
+//   console.log(`${country} is ${countryAvg} million below average`);
+// }
