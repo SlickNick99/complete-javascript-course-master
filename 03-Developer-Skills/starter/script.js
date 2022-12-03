b27da2ae1dff02738bf96871b698365dce1be0bb
SlickNick99 <59777492+SlickNick99@users.noreply.github.com>
2022-12-03 12:24:19 -0800

    beds sold

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -132,38 +132,50 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // add the months in the same function but a different for loop then possibly combine both of them into a log statement
 
 // logging the whole string
-console.log(
-  'In the month of January 34 mattreses were sold and 2104 were sold in total'
-);
-const bedsSoldMonthly = [34, 200, 188, 19, 10, 78, 93, 205, 609, 500, 78];
+// console.log(
+//   'In the month of January 34 mattreses were sold and 2104 were sold in total'
+// );
+// const bedsSoldMonthly = [34, 200, 188, 19, 10, 78, 93, 205, 609, 500, 78, 100];
 
-const months = [
-  'January',
-  'Februrary',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-];
+// const allSoldBeds = function (beds) {
+//   let allBeds = 0;
+//   for (let i = 0; i < beds.length; i++) {
+//     allBeds += beds[i];
+//   }
 
-const salesAndMonths = function (sales, months) {
-  let totalSales = 0;
-  for (let i = 0; i < sales.length && i < months.length; i++) {
-    totalSales += sales[i];
-    console.log(
-      `In the month of ${months[i]} ${sales[i]} were sold and ${totalSales} were sold this year` +
-        `${totalSales}`
-    );
-  }
-  return totalSales;
-};
+//   return allBeds;
+// };
 
-salesAndMonths(bedsSoldMonthly, months);
+// allSoldBeds(bedsSoldMonthly);
+
+// const months = [
+//   'January',
+//   'Februrary',
+//   'March',
+//   'April',
+//   'May',
+//   'June',
+//   'July',
+//   'August',
+//   'September',
+//   'October',
+//   'November',
+//   'december',
+// ];
+
+// const salesAndMonths = function (sales, months) {
+//   let totalSales = 0;
+//   for (let i = 0; i < sales.length && i < months.length; i++) {
+//     totalSales += sales[i];
+//     console.log(
+//       `In the month of ${months[i]} ${sales[i]} were sold and ${totalSales} were sold this year` +
+//         `${allSoldBeds(bedsSoldMonthly)}`
+//     );
+//   }
+//   return totalSales;
+// };
+
+// salesAndMonths(bedsSoldMonthly, months);
 // const bedSales = function (a) {
 //   let totalBeds = 0;
 
@@ -187,3 +199,49 @@ salesAndMonths(bedsSoldMonthly, months);
 // monthsOfYear(months);
 
 // bedSales(bedsSoldMonthly);
+
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+
+// const calcTips = function (bills) {
+//   let tipLeft = 0;
+//   let totalBill = 0;
+
+//   for (let i = 0; i < bills.length; i++) {
+//     if (bills[i] > 50 && bills[i] < 300) {
+//       tipLeft = bills[i] * 0.15;
+//     } else {
+//       tipLeft = bills[i] * 0.2;
+//     }
+//     totalBill += bills[i] + tipLeft;
+//     tips.push(tipLeft);
+//     totals.push(totalBill);
+//     const total = Math.round(tips[i]) + totals[i];
+
+//     console.log(total);
+//   }
+//   // return total;
+// };
+// const calcAvg = function (a) {
+//   let avg = 0;
+
+//   for (let i = 0; i < a.length; i++) {
+//     avg += a[i];
+//   }
+
+//   // return avg;
+//   avg /= a.length;
+//   console.log(avg);
+//   return avg;
+// };
+
+// // console.log(tips);
+// // console.log(totals);
+// // console.log(calcTips(bills));
+
+// calcTips(bills);
+// calcAvg(bills);
+// console.log(tips);
+// console.log(totals);
+// console.log(calcAvg(totals));
