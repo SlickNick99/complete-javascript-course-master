3b85a5fd22ab0a369a7cdc454daf2c667bd7432a
SlickNick99 <59777492+SlickNick99@users.noreply.github.com>
2022-12-16 12:39:52 -0800

    This keyword

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,95 @@
 'use strict';
+
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+//   console.log(firstName);
+//   function printAge() {
+//     const output = `You are ${age}, born in ${birthYear}`;
+//     console.log(output);
+
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       const str = console.log('You are a millenial');
+//     }
+//   }
+
+//   printAge(1994);
+//   return age;
+// }
+// const firstName = 'Jonas';
+// console.log(calcAge(1991));
+
+// Functions
+
+// console.log(addDecl(4, 5));
+// console.log(addExpress(4, 5));
+// console.log(addArr(4, 5));
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpress = function (a, b) {
+//   return a + b;
+// };
+// const addArr = (a, b) => a + b;
+
+const jonas = {
+  firstName: 'jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1
+    // const self = this;
+    // const isMillenial = function () {
+    //   console.log(
+    //     self.year >= 1991 && self.year <= 1996
+    //       ? 'You are a millenial'
+    //       : 'You are not a millenial'
+    //   );
+    // };
+
+    // Solution 2
+    const isMillenial = () => {
+      console.log(
+        self.year >= 1991 && self.year <= 1996
+          ? 'You are a millenial'
+          : 'You are not a millenial'
+      );
+    };
+    isMillenial();
+  },
+  greet: function () {
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+jonas.greet();
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+jonas.calcAge();
+
+// Arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+
+addExpr(2, 5);
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+console.log(age);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+const friend = me;
+friend.age = 27;
